686c90346628bebac47324faf978dc8c4f67c8d1
pablocalderon9408 <pablocalderon9408@gmail.com>
2021-09-11 23:15:27 -0500

    User and profile model created

--- config/settings/base.py
+++ config/settings/base.py
@@ -30,6 +30,12 @@ ROOT_URLCONF = 'config.urls'
 # WSGI
 WSGI_APPLICATION = 'config.wsgi.application'
 
+#Users & Authentication
+AUTH_USER_MODEL = 'users.User'
+
+DEFAULT_AUTO_FIELD='django.db.models.AutoField'
+
+
 # Apps
 DJANGO_APPS = [
     'django.contrib.auth',
@@ -43,6 +49,7 @@ DJANGO_APPS = [
 THIRD_PARTY_APPS = [
 ]
 LOCAL_APPS = [
+    'cride.users.apps.UsersAppConfig',
 ]
 INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + LOCAL_APPS
 
